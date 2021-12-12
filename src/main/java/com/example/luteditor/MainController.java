ceb9f6d0fb01a2a1e5fa39c072d6a2b018d3c417
Luke460 <furlanoluca@libero.it>
2021-12-12 18:48:25 +0100

    Working lut chart

--- src/main/java/com/example/luteditor/MainController.java
+++ src/main/java/com/example/luteditor/MainController.java
@@ -7,12 +7,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.text.DecimalFormat;
+import java.util.Objects;
+
 import org.apache.commons.math3.util.Precision;
 
 public class MainController {
@@ -33,15 +36,17 @@ public class MainController {
     private TextField gammaTextField;
     @FXML
     private Slider gammaSlider;
+    @FXML
+    private LineChart lineChart;
 
     private static final String CALIBRATION_SCENE_FILE = "CalibrationScene.fxml";
     private static final String MAIN_EDITING_FILE = "MainEditingScene.fxml";
-    DecimalFormat df = new DecimalFormat("###.##");
+    private static final Integer LUT_RESOLUTION = 100;
 
     // SCENE SWITCHING
 
     public void switchToNewCalibrationScene(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(CALIBRATION_SCENE_FILE));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(CALIBRATION_SCENE_FILE)));
         Node sourceNode = (Node)event.getSource();
         Scene sourceScene = sourceNode.getScene();
         stage = (Stage)sourceScene.getWindow();
@@ -51,7 +56,7 @@ public class MainController {
     }
 
     public void switchToMainEditingScene(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource(MAIN_EDITING_FILE));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(MAIN_EDITING_FILE)));
         Node sourceNode = (Node)event.getSource();
         Scene sourceScene = sourceNode.getScene();
         stage = (Stage)sourceScene.getWindow();
@@ -65,46 +70,77 @@ public class MainController {
     // dead zone
     public void updateDeadZoneFromTextField(Event event) throws  IOException{
         String stringValue = deadZoneTextField.getText();
-        double value = Precision.round(Double.valueOf(stringValue),2);
+        double value = Precision.round(Double.parseDouble(stringValue),2);
         deadZoneSlider.adjustValue(value);
         // auto value cast
         deadZoneTextField.setText(""+Precision.round(deadZoneSlider.getValue(),2));
+        // update graph
+        updateChart();
     }
 
     public void updateDeadZoneFieldFromSlider(Event event) throws IOException{
         double value = Precision.round(deadZoneSlider.getValue(),2);
         deadZoneTextField.setText(Double.toString(value));
+        // update graph
+        updateChart();
     }
 
     // gain
     public void updateGainFromTextField(Event event) throws IOException{
         String stringValue = gainTextField.getText();
-        long value = Math.round(Double.valueOf(stringValue));
+        long value = Math.round(Double.parseDouble(stringValue));
         gainSlider.adjustValue(value);
         // auto value cast
         gainTextField.setText(""+Math.round(gainSlider.getValue()));
+        // update graph
+        updateChart();
     }
 
     public void updateGainFieldFromSlider(Event event) throws IOException{
         long value = Math.round(gainSlider.getValue());
         gainTextField.setText(Long.toString(value));
+        // update graph
+        updateChart();
     }
 
     // gamma
     public void updateGammaFromTextField(Event event) throws IOException{
         String stringValue = gammaTextField.getText();
-        double value = Precision.round(Double.valueOf(stringValue),2);
+        double value = Precision.round(Double.parseDouble(stringValue),2);
         gammaSlider.adjustValue(value);
         gammaTextField.setText(""+Precision.round(gammaSlider.getValue(),2));
+        // update graph
+        updateChart();
     }
 
     public void updateGammaFieldFromSlider(Event event) throws IOException{
         double value = Precision.round(gammaSlider.getValue(),2);
         gammaTextField.setText(Double.toString(value));
+        // update graph
+        updateChart();
+    }
+
+    // default values
+    public void setDefaultValues(){
+        deadZoneTextField.setText("0");
+        deadZoneSlider.setValue(0);
+        gainTextField.setText("100");
+        gainSlider.setValue(100);
+        gammaTextField.setText("0");
+        gammaSlider.setValue(0);
+        // update graph
+        updateChart();
     }
 
     public void updateChart() {
-        // TODO
+        XYChart.Series lut = utils.Calculator.generateCustomLut(
+                LUT_RESOLUTION,
+                Double.parseDouble(deadZoneTextField.getText()),
+                Integer.parseInt(gainTextField.getText()),
+                Double.parseDouble(gammaTextField.getText())
+        );
+        lineChart.getData().clear();
+        lineChart.getData().add(lut);
     }
 
 }
